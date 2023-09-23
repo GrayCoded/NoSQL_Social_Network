1df47644356e82c71179c2ea1eb962a115f4b674
GrayCoded <130934177+GrayCoded@users.noreply.github.com>
2023-09-23 16:03:35 -0300

    worked on getting thoughts model finished

--- models/Thought.js
+++ models/Thought.js
@@ -1,12 +1,18 @@
+const dayjs = require('dayjs');
 const { Schema, Types } = require('mongoose');
 
-//creating the thought SCHEMA
+//creating the Thought SCHEMA
 
 const thoughtSchema = new Schema(
     {
         thoughtId: {
             type: Schema.Types.ObjectId,
             default: () => new Types.ObjectId(),
+        },
+        thoughtText: {
+            type: String,
+            required: true,
+            maxlength: 280,
         },
         createdAt: {
             type: Date,
@@ -29,4 +35,44 @@ const thoughtSchema = new Schema(
     }
 );
 
-module.exports = thoughtSchema;
+//Creating the Reaction SCHEMA
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+        },
+        responseBody: {
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdOnDate) => dayjs(createdOnDate).format('YYYY-MM-DD')
+        },
+    },
+    {
+        toJSON: {
+            vitual: true,
+            getters: true,
+
+        },
+        id: false,
+    }
+);
+//Creating a vitual Property
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions ? this.reactions.length : 0;
+});
+
+// Initialize our Thought model
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -4,30 +4,43 @@ const { Schema, model } = require('mongoose');
 // Schema to create User model
 const userSchema = new Schema(
   {
-    first: String,
-    last: String,
-    age: Number,
-    videos: [
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trimmed: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        'Please enter a valid email address.',
+      ],
+    },
+    thoughts: [
       {
         type: Schema.Types.ObjectId,
-        ref: 'video',
+        ref: 'thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
       },
     ],
   },
-  {
-    // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
-    // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
-    toJSON: {
-      virtuals: true,
-    },
-    id: false,
-  }
-);
-
+{
+  toJSON: {
+    virtuals: true,
+  },
+  id: false,
+});
 // Create a virtual property `friendCount` that gets and sets the user's friends
 userSchema
   .virtual('friendCount')
-  // Getter
   .get(function () {
     return this.friends ? this.friends.length : 0;
   });
